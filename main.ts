3b799f93d3757b7a260ae6055e0cbe3d5b36f9c9
grinnan <cgrinnan@gmail.com>
2021-12-31 13:12:28 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 makerbit.connectLcd(39)
+I2C_LCD1602.LcdInit(39)
+I2C_LCD1602.i2cLcdInit(39)
+I2C_LCD1602.i2cLcdOn()
+I2C_LCD1602.BacklightOn()
+I2C_LCD1602.i2cLcdBacklightOn()
 if (makerbit.isLcdConnected()) {
     basic.showString("Connected to LCD")
 } else {
@@ -9,6 +14,8 @@ for (let index = 0; index < 4; index++) {
     makerbit.showStringOnLcd1602("MakerBit", makerbit.position1602(LcdPosition1602.Pos1), 16)
     basic.pause(1000)
     basic.showString("Hello!")
+    I2C_LCD1602.ShowString("Hello", 0, 0)
     basic.pause(1000)
+    I2C_LCD1602.i2cLcdShowString("", 0, 0)
     makerbit.clearLcd1602()
 }
